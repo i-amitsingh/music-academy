cedbda7872af6718ef2aadf26ee2c717643f787a
AMIT SINGH <amitsingh@AMITs-MacBook-Air.local>
2024-07-16 18:45:38 +0530

    init

--- src/components/UpcomingWebinars.tsx
+++ src/components/UpcomingWebinars.tsx
@@ -60,19 +60,21 @@ function UpcomingWebinars() {
 
         <div className="mt-10">
           <HoverEffect
-          items={featuredWebinars.map(webinar => (
-            {
-              title: webinar.title,
-              description: webinar.description,
-              link: '/'
-            }
-          ))}
+
+            items={featuredWebinars.map(webinar => (
+              {
+                title: webinar.title,
+                description: webinar.description,
+                link: '/'
+              }
+
+            ))}
           />
         </div>
 
         <div className="mt-10 text-center">
           <Link href={"/"}
-          className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
+            className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
           >
             View All webinars
           </Link>
